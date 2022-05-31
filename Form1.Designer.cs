604477c5613100bcabef3fee670b12e20081cc7a
Home2 <Home2@HOME2-PC>
2022-05-30 23:11:50 -0500

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace WindowsFormsApp1_Termometro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button_Salir = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.buttonOn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonOff = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button_Salir
+            // 
+            this.button_Salir.Location = new System.Drawing.Point(693, 340);
+            this.button_Salir.Name = "button_Salir";
+            this.button_Salir.Size = new System.Drawing.Size(75, 23);
+            this.button_Salir.TabIndex = 1;
+            this.button_Salir.Text = "Salir";
+            this.button_Salir.UseVisualStyleBackColor = true;
+            this.button_Salir.Click += new System.EventHandler(this.button_Salir_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.BackColor = System.Drawing.SystemColors.WindowText;
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.ForeColor = System.Drawing.SystemColors.Window;
+            this.numericUpDown1.Location = new System.Drawing.Point(487, 87);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            40,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 44);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // buttonOn
+            // 
+            this.buttonOn.Location = new System.Drawing.Point(0, 0);
+            this.buttonOn.Name = "buttonOn";
+            this.buttonOn.Size = new System.Drawing.Size(75, 23);
+            this.buttonOn.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(477, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 68);
+            this.label1.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(560, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(164, 68);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "°C";
+            // 
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.BaudRate = 115200;
+            this.serialPort1.PortName = "COM16";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label3.Location = new System.Drawing.Point(341, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 64);
+            this.label3.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::WindowsFormsApp1_Termometro.Properties.Resources.grados;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonOff);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonOn);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button_Salir);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_Salir;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button buttonOn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonOff;
+        private System.Windows.Forms.Timer timer1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1_Termometro
+{
+    
+    public partial class Form1 : Form
+    {
+
+        double cpx = 98;
+        double cpy = 40;
+        double hipo = 60;
+        double angulo = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void button_Salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            Image newImage = Image.FromFile("C:\\Users\\chino\\Downloads\\grados.gif");
+            Pen plumaAzul = new Pen(Color.Blue, 3f);
+            this.Refresh();
+            Graphics superficie = CreateGraphics();
+            angulo = (double)numericUpDown1.Value-360;
+            angulo = (angulo + 20.0) * 2.5;
+            cpx = (Math.Cos(angulo*(Math.PI/180)) * hipo) + 98.0F;
+            cpy = (Math.Sin(angulo*(Math.PI/180)) * hipo) + 100.0F;
+            this.Text = angulo.ToString();
+
+            PointF punto1 = new PointF((float) cpx, (float) cpy);
+            PointF punto2 = new PointF(98.0F, 100.0F);
+            
+            //superficie.DrawImage(newImage, 84, 84, 234, 234);
+            superficie.DrawLine(plumaAzul, punto1, punto2);
+            superficie.Dispose();
+            label1.Text = (numericUpDown1.Value + 20).ToString();
+        }
+
+      
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!serialPort1.IsOpen)
+            {
+                serialPort1.Open();
+            }
+            else
+            {
+                string buffer;
+                buffer = serialPort1.ReadLine();
+                buffer = buffer.Substring(0,2);
+                if(buffer.Length <= 8)
+                {
+                    int temp = Convert.ToInt32(buffer);
+                    numericUpDown1.Value = temp;
+                    label3.Text = buffer + " " + "*C";
+                    //this.Refresh();
+                }
+            }
+        }
+    }
+}
